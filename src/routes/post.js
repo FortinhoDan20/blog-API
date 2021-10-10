fc2073d271cb54b470c9e56da51025ee2b5af012
FortinhoDan20 <fortinhodanvhamba@gmail.com>
2021-10-10 11:51:42 +0100

    ajout de la route detail post

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { getAllPosts, deletePost, createPost, updatePost } = require('../controllers/post')
+const { getAllPosts, deletePost, createPost, updatePost, getPost } = require('../controllers/post')
 const router = new express.Router()
 
 router.post('/add', createPost)
 
 router.get('/', getAllPosts)
+
+router.get('/:id', getPost)
 
 router.patch('/:id', updatePost)
 
